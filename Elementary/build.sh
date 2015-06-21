b1aea932471f7bc8085f46aa1f203153ea3e505d
Peng Wang <peng@pengxwang.com>
2015-06-21 13:58:33 -0700

    Elementary: add custom updating of template deps.

---
+++ Elementary/build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Run from project root.
+
+cp -v bower_components/dat-gui/build/dat.gui.min.js Elementary/template-coffee/
+cp -v bower_components/coffeescript/extras/coffee-script.js Elementary/template-coffee/
