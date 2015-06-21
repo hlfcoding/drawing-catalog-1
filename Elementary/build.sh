62443e9d482290b078fd8595fc5115cc644f8852
Peng Wang <peng@pengxwang.com>
2015-06-21 14:52:20 -0700

    Fixup 3e449d5, revert to using Processing.js 1.4.1.

--- Elementary/build.sh
+++ Elementary/build.sh
@@ -4,4 +4,4 @@
 
 cp -v bower_components/dat-gui/build/dat.gui.min.js Elementary/template-coffee/
 cp -v bower_components/coffeescript/extras/coffee-script.js Elementary/template-coffee/
-cp -v bower_components/Processing.js/processing.min.js Elementary/template-coffee/
+cp -v bower_components/Processing.js/processing.js Elementary/template-coffee/
